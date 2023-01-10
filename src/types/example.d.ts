d8793a663bdec1b0db792ff63e53d6998bf771e9
0xjimmy <contact@decentralised.tech>
2023-01-11 10:26:59 +1300

    Types

--- src/types/example.d.ts
+++ src/types/example.d.ts
@@ -18,7 +18,7 @@
  * ```
  */
 declare module 'module-name' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const whatever: any;
-  export = whatever;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const whatever: any;
+	export = whatever;
 }
